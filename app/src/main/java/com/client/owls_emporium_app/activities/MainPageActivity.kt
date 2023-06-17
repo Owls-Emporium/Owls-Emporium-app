a2d9df865e589035c4c053d6f3e7ad81b2103577
rodolfo <00082421@uca.edu.sv>
2023-06-17 17:12:14 -0600

    add logout :sparkles:

--- app/src/main/java/com/client/owls_emporium_app/activities/MainPageActivity.kt
+++ app/src/main/java/com/client/owls_emporium_app/activities/MainPageActivity.kt
@@ -1,7 +1,9 @@
 package com.client.owls_emporium_app.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.client.owls_emporium_app.R
@@ -17,12 +19,18 @@ class MainPageActivity : AppCompatActivity() {
 
     private val TAG ="MainPageActivity"
     var bottomNavigation: BottomNavigationView? = null
+    var buttonLogout: Button ?= null
+    var sharedPref: SharedPref ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
 
+        sharedPref = SharedPref(this)
+        buttonLogout = findViewById(R.id.btn_logout)
+        buttonLogout?.setOnClickListener{ logout() }
         getUserFromSession()
+
         openFragment(MainPageFragment())
 
         bottomNavigation = findViewById(R.id.btn_navigation)
@@ -49,22 +57,26 @@ class MainPageActivity : AppCompatActivity() {
 
 
     }
+    //para cerrar sesion del usuario, borrar key
+    private fun logout(){
+        sharedPref?.remove("user")
+        val i = Intent(this, MainActivity::class.java)
+        startActivity(i)
+    }
+    //para mantener la sesion activa
+    private fun getUserFromSession(){
+        val gson =Gson()
+
+        if(!sharedPref?.getData("user").isNullOrBlank()){
+            //si el usuario exite en sesion
+            val user = gson.fromJson(sharedPref?.getData("user"), User::class.java)
+            Log.d(TAG,"Usuario: $user")
+        }
+    }
     private fun openFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.container, fragment)
         transaction.addToBackStack(null)
         transaction.commit()
-    }
-    //para mantener la sesion activa
-    private fun getUserFromSession(){
-
-        val sharedPref =SharedPref(this)
-        val gson =Gson()
-
-        if(!sharedPref.getData("user").isNullOrBlank()){
-            //si el usuario exite en sesion
-            val user = gson.fromJson(sharedPref.getData("user"), User::class.java)
-            Log.d(TAG,"Usuario: $user")
-        }
     }
 }
