fb296c00cfc4807e4b912a7a020efb5aab776bd9
rodolfo <00082421@uca.edu.sv>
2023-06-29 10:04:52 -0600

    fix_a_litle_depende_in_settings:ambulance:

--- app/src/main/java/com/client/owls_emporium_app/activities/UpdateProfileActivity.kt
+++ app/src/main/java/com/client/owls_emporium_app/activities/UpdateProfileActivity.kt
@@ -2,11 +2,18 @@ package com.client.owls_emporium_app.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.client.owls_emporium_app.R
+import de.hdodenhof.circleimageview.CircleImageView
 
 class UpdateProfileActivity : AppCompatActivity() {
+
+    var circleImageUser : CircleImageView?= null
+    var btn_update : Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_profile)
+
     }
 }
--- app/src/main/java/com/client/owls_emporium_app/fragments/ProfileFragment.kt
+++ app/src/main/java/com/client/owls_emporium_app/fragments/ProfileFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.client.owls_emporium_app.R
+import de.hdodenhof.circleimageview.CircleImageView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,11 +23,13 @@ class ProfileFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
