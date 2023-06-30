ebec2b4c9b100c8c73dc18beb365a23a697cf403
Vanegas1 <00258621@uca.edu.sv>
2023-06-30 15:26:17 -0600

    add cardview posts created :iphone:

---
+++ app/src/main/java/com/client/owls_emporium_app/activities/PostsCreatedActivity.kt
@@ -0,0 +1,12 @@
+package com.client.owls_emporium_app.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.client.owls_emporium_app.R
+
+class PostsCreatedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_posts_created)
+    }
+}
