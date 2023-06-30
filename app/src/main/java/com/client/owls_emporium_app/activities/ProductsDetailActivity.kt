97c3929ee18afb031fd5625c4e22a3604f1c28fa
Vanegas1 <00258621@uca.edu.sv>
2023-06-30 14:45:25 -0600

    add view products detail :iphone:

---
+++ app/src/main/java/com/client/owls_emporium_app/activities/ProductsDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.client.owls_emporium_app.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.client.owls_emporium_app.R
+
+class ProductsDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_products_detail)
+    }
+}
