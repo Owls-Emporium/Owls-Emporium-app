b2889af8b50c6d89a35da9ea313d477102347708
Kemc17 <00129421@uca.edu.sv>
2023-07-02 23:14:52 -0600

    Add Logout function :safety_vest:

--- app/src/main/java/com/client/owls_emporium_app/fragments/ProfileFragment.kt
+++ app/src/main/java/com/client/owls_emporium_app/fragments/ProfileFragment.kt
@@ -10,6 +10,7 @@ import android.widget.Button
 import android.widget.TextView
 import com.client.owls_emporium_app.R
 import com.bumptech.glide.Glide
+import com.client.owls_emporium_app.activities.MainActivity
 import com.client.owls_emporium_app.network.models.User
 import com.client.owls_emporium_app.network.utils.SharedPref
 import com.google.gson.Gson
@@ -23,7 +24,7 @@ class ProfileFragment : Fragment() {
     var textViewName: TextView?= null
     var textViewEmail: TextView ?= null
     var textViewPhone: TextView ?= null
-
+    var buttonLogout: Button ?= null
     var sharedPref: SharedPref ?= null
     var user: User?= null
 
@@ -35,22 +36,32 @@ class ProfileFragment : Fragment() {
         myview = inflater.inflate(R.layout.fragment_profile, container, false)
 
         sharedPref = SharedPref(requireActivity())
-
         buttonUpdateProfile = myview?.findViewById(R.id.btn_update_profile)
         textViewName = myview?.findViewById(R.id.textview_name1)
         textViewEmail = myview?.findViewById(R.id.textview_email1)
         textViewPhone = myview?.findViewById(R.id.textview_phone1)
         circleImageUser = myview?.findViewById(R.id.circleimage_user)
+        buttonLogout = myview?.findViewById(R.id.btn_logout)
+
+        buttonLogout?.setOnClickListener{ logout() }
 
         getUserFromSession()
 
         textViewName?.text = "${user?.name} ${user?.lastname}"
         textViewEmail?.text = user?.email
         textViewPhone?.text = user?.phone
-        Glide.with(requireContext()).load(user?.image).into(circleImageUser!!)
 
+        if (!user?.image.isNullOrBlank()) {
+            Glide.with(requireContext()).load(user?.image).into(circleImageUser!!)
+        }
 
         return myview
+    }
+
+    private fun logout() {
+        sharedPref?.remove("user")
+        val i = Intent(requireActivity(), MainActivity::class.java)
+        startActivity(i)
     }
 
     //obtiene los datos de session
